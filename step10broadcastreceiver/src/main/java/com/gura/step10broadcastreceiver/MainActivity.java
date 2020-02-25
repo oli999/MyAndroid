ac6d3c4190eadb9d2df04dea73de2eeb59cb94a5
kimgura <oli999@naver.com>
2020-02-25 10:58:45 +0900

    step10broadcastreceiver 모듈 수정함

--- step10broadcastreceiver/src/main/java/com/gura/step10broadcastreceiver/MainActivity.java
+++ step10broadcastreceiver/src/main/java/com/gura/step10broadcastreceiver/MainActivity.java
@@ -5,16 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //TextView 의 참조값을 담을 필드
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TextView 의 참조값을 필드에 저장
+        textView=findViewById(R.id.textView);
 
+        //방송 수신자 객체 생성해서
         MyReceiver mr=new MyReceiver();
+        //동작이 가능하도록 등록하기
         registerReceiver(mr,
                 new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED));
+        //방송 수신자 객체에 MainActivity 의 참조값 전달하기
+        mr.setActivity(this);
+    }
+    //방송 수신자 객체가 호출할 메소드
+    public void updateUI(String msg){
+        textView.setText(msg);
     }
 }
+
+
+
+
+
+
+
--- step10broadcastreceiver/src/main/java/com/gura/step10broadcastreceiver/MyReceiver.java
+++ step10broadcastreceiver/src/main/java/com/gura/step10broadcastreceiver/MyReceiver.java
@@ -13,11 +13,28 @@ import android.widget.Toast;
     - AndroidManifest.xml 에 등록을 해야한다.
  */
 public class MyReceiver extends BroadcastReceiver {
-    //특정 방송이 수신되면 호출되는 메소드
+    //액티비티의 참조값을 담을 필드
+    private MainActivity activity;
+    //액티비티의 참조값을 필드에 저장하는 setter 메소드
+    public void setActivity(MainActivity activity) {
+        this.activity = activity;
+    }
+
+    //특정 방송(비행기 모드가 on 또는 off 되었을때)이 수신되면 호출되는 메소드
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "방송이 수신되었네?",
-                Toast.LENGTH_LONG).show();
+
+        boolean isOn=intent.getBooleanExtra("state", false);
+        if(isOn){
+            Toast.makeText(context, "비행모드가 켜졌네?",
+                    Toast.LENGTH_LONG).show();
+            activity.updateUI("AirPlaneMode On");
+        }else{
+            Toast.makeText(context, "비행모드가 꺼졌네?",
+                    Toast.LENGTH_LONG).show();
+            activity.updateUI("AirPlaneMode Off");
+        }
+
     }
 }
 
