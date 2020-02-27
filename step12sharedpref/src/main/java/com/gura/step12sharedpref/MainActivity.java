1bea50fd86bf4ad3daadbcb83592463080de1c6e
kimgura <oli999@naver.com>
2020-02-27 11:17:09 +0900

    step12sharedpref 기능 추가함

--- step12sharedpref/src/main/java/com/gura/step12sharedpref/MainActivity.java
+++ step12sharedpref/src/main/java/com/gura/step12sharedpref/MainActivity.java
@@ -1,19 +1,26 @@
 package com.gura.step12sharedpref;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+                            implements View.OnClickListener{
+    //필요한 필드
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,21 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        //EditText 참조값 얻어와서 필드에 저장하기
+        editText=findViewById(R.id.editText);
+        //Button 의 참조값 얻어와서 리스너 등록하기
+        Button saveBtn=findViewById(R.id.saveBtn);
+        saveBtn.setOnClickListener(this);
+
+        SharedPreferences pref=
+                getSharedPreferences("info", MODE_PRIVATE);
+        // .getString( key 값, default 값)
+        String myName=pref.getString("myName", "empty");
+        if(myName.equals("empty")){
+            return;
+        }
+        //저장된 이름을 editText 에 출력하기
+        editText.setText(myName);
     }
 
     @Override
@@ -56,4 +78,34 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    @Override
+    public void onClick(View view) {
+        //입력한 문자열 읽어오기
+        String inputName=editText.getText().toString();
+        //SharedPreferences 를 활용해서 저장하기
+
+        SharedPreferences pref=
+                getSharedPreferences("info", MODE_PRIVATE);
+        //Editor 객체의 참조값을 얻어와서
+        SharedPreferences.Editor editor=pref.edit();
+        //myName 이라는 키값으로 문자열 저장하기
+        editor.putString("myName", inputName);
+        editor.commit(); //실제 저장되는 시점
+        //알림 띄우기
+        new AlertDialog.Builder(this)
+                .setMessage("저장 했습니다.")
+                .setNeutralButton("확인", null)
+                .show();
+
+    }
 }
+
+
+
+
+
+
+
+
+
--- step12sharedpref/src/main/java/com/gura/step12sharedpref/SettingsActivity.java
+++ step12sharedpref/src/main/java/com/gura/step12sharedpref/SettingsActivity.java
@@ -12,20 +12,29 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //res/layout/settings_activity.xml 문서를 전개해서 화면 구성하기
         setContentView(R.layout.settings_activity);
+        /*
+            id 가 settings 인 레이아웃(FrameLayout) 에
+            SettingsFragment 로 화면 구성하게 하기
+         */
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment())
                 .commit();
+
+        // 좌상단 up 버튼 동작 가능하도록 한다.
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
-
+    //FrameLayout 을 채출 프레그먼트
     public static class SettingsFragment extends PreferenceFragmentCompat {
+        //프레그먼트가 처음 활성화 될때 호출되믄 메소드
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            // res/xml/root_preferences.xml 문서를 전개해서 프레그먼트 UI 구성하기
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
 
         }
